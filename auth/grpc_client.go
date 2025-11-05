7344e8b47eb83ce781c97a15d6a95bf40a1b0c1c
DraconDev <dracsharp@gmail.com>
2025-11-05 01:38:20 +0000

    fix(auth): update import path for cerberus package

--- auth/grpc_client.go
+++ auth/grpc_client.go
@@ -10,7 +10,7 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
 
-	auth_cerberus "./cerberus"
+	"star/auth/cerberus"
 )
 
 // GRPCAuthClient handles communication with the Cerberus authentication service via gRPC
