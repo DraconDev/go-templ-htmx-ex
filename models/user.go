4444788ac3b1522eed224ddda4e8782d00187b3f
DraconDev <dracsharp@gmail.com>
2025-11-09 02:55:25 +0000

    feat(models): rename User to UserSession and add AuthResponse, SetSessionRequest

--- models/user.go
+++ models/user.go
@@ -1,21 +1,5 @@
 package models
 
-// User represents a user in the system
-type User struct {
-	ID       string `json:"id"`
-	Email    string `json:"email"`
-	Name     string `json:"name"`
-	Picture  string `json:"picture"`
-	LoggedIn bool   `json:"logged_in"`
-}
-
-// Config holds application configuration
-type Config struct {
-	ServerPort     string
-	AuthServiceURL string
-	RedirectURL    string
-}
-
 // UserSession represents a logged-in user session
 type UserSession struct {
 	ID       string `json:"id"`
@@ -23,4 +7,21 @@ type UserSession struct {
 	Name     string `json:"name"`
 	Picture  string `json:"picture"`
 	LoggedIn bool   `json:"logged_in"`
+}
+
+// AuthResponse represents the response from the auth service
+type AuthResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Token   string `json:"token,omitempty"`
+	UserID  string `json:"user_id,omitempty"`
+	Email   string `json:"email,omitempty"`
+	Name    string `json:"name,omitempty"`
+	Picture string `json:"picture,omitempty"`
+	Error   string `json:"error,omitempty"`
+}
+
+// SetSessionRequest represents a request to set a session token
+type SetSessionRequest struct {
+	Token string `json:"token"`
 }
