aff5ba70f2e1a847e52040a212b05d552c3baa05
DraconDev <dracsharp@gmail.com>
2025-11-12 03:49:38 +0000

    feat(auth): Remove unused templates import

--- handlers/auth.go
+++ handlers/auth.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/DraconDev/go-templ-htmx-ex/auth"
 	"github.com/DraconDev/go-templ-htmx-ex/config"
-	"github.com/DraconDev/go-templ-htmx-ex/templates"
+	
 )
 
 // =============================================================================
