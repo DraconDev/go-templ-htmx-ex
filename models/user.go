ae350f442561885bf51f1d51e5e2890c8ae51430
DraconDev <dracsharp@gmail.com>
2025-11-14 22:05:35 +0000

    feat(user): Remove unused AuthResponse struct

--- models/user.go
+++ models/user.go
@@ -9,19 +9,6 @@ type UserSession struct {
 	LoggedIn bool   `json:"logged_in"`
 }
 
-// AuthResponse represents the response from the auth service
-type AuthResponse struct {
-	Success      bool   `json:"success"`
-	Message      string `json:"message"`
-	Token        string `json:"token,omitempty"`
-	RefreshToken string `json:"refresh_token,omitempty"`
-	UserID       string `json:"user_id,omitempty"`
-	Email        string `json:"email,omitempty"`
-	Name         string `json:"name,omitempty"`
-	Picture      string `json:"picture,omitempty"`
-	Error        string `json:"error,omitempty"`
-}
-
 // TokenExchangeResponse represents the response from exchanging auth code for session
 type TokenExchangeResponse struct {
 	Success     bool   `json:"success"`
