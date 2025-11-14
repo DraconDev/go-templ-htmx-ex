17fc0778b1319196b88eddba24711dea6540e9bb
DraconDev <dracsharp@gmail.com>
2025-11-14 22:08:19 +0000

    feat(models): Add AuthResponse struct and remove JWTClaims Adds a new `AuthResponse` struct to the `models` package and removes the now unused `JWTClaims` struct.

--- models/user.go
+++ models/user.go
@@ -1,23 +1,23 @@
 package models
 
+// AuthResponse represents the response from the auth service
+type AuthResponse struct {
+	Success  bool   `json:"success"`
+	Message  string `json:"message"`
+	Token    string `json:"token,omitempty"`
+	UserID   string `json:"user_id,omitempty"`
+	Email    string `json:"email,omitempty"`
+	Name     string `json:"name,omitempty"`
+	Picture  string `json:"picture,omitempty"`
+	Error    string `json:"error,omitempty"`
+}
+
 // TokenExchangeResponse represents the response from exchanging auth code for session
 type TokenExchangeResponse struct {
 	Success     bool   `json:"success"`
 	Message     string `json:"message"`
 	IdToken     string `json:"id_token"` // Server session token
 	Error       string `json:"error,omitempty"`
-}
-
-// JWTClaims represents the standard OpenID Connect claims in a JWT
-type JWTClaims struct {
-	Subject string `json:"sub"`    // User ID
-	Name    string `json:"name"`   // Full name
-	Email   string `json:"email"`  // Email address
-	Picture string `json:"picture"` // Avatar URL
-	Issuer  string `json:"iss"`    // Issuer (auth service)
-	Audience string `json:"aud"`   // Audience
-	Expires int64  `json:"exp"`    // Expiration time
-	Issued  int64  `json:"iat"`    // Issued at
 }
 
 // ExchangeCodeRequest represents a request to exchange authorization code for tokens
