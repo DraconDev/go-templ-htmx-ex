590cf457197923eb33ff37df3b1c58af65b6bc11
DraconDev <dracsharp@gmail.com>
2025-11-15 02:55:17 +0000

    feat(auth): Remove token from AuthResponse struct

--- models/user.go
+++ models/user.go
@@ -4,7 +4,7 @@ package models
 type AuthResponse struct {
 	Success  bool   `json:"success"`
 	Message  string `json:"message"`
-	Token    string `json:"token,omitempty"`
+	
 	UserID   string `json:"user_id,omitempty"`
 	Email    string `json:"email,omitempty"`
 	Name     string `json:"name,omitempty"`
