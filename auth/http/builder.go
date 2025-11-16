cb1c7e5c5590df37ecc2f2339dd083c8f15def86
DraconDev <dracsharp@gmail.com>
2025-11-16 01:44:31 +0000

    feat: update 3 files (.vscode/code-counter/reports/code-counter-report.html, auth/http/builder.go, auth/http/parser.go) at 01:44

--- auth/http/builder.go
+++ auth/http/builder.go
@@ -3,11 +3,26 @@ package http
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/DraconDev/go-templ-htmx-ex/auth/errors"
 )
+
+// AuthServiceError represents a basic auth service error for HTTP package
+type AuthServiceError struct {
+	Code    string
+	Message string
+	Err     error
+}
+
+func (e *AuthServiceError) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("auth service error: %s - %v", e.Message, e.Err)
+	}
+	return fmt.Sprintf("auth service error: %s", e.Message)
+}
 
 // Request represents a built HTTP request
 type Request struct {
@@ -35,12 +50,12 @@ func NewRequestBuilder(secret string) *RequestBuilder {
 func (rb *RequestBuilder) BuildPOSTRequest(endpoint string, params map[string]string) (*Request, error) {
 	jsonData, err := json.Marshal(params)
 	if err != nil {
-		return nil, NewAuthServiceError("INVALID_PARAMS", "Failed to marshal request data", err)
+		return nil, &AuthServiceError{"INVALID_PARAMS", "Failed to marshal request data", err}
 	}
 
 	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(jsonData))
 	if err != nil {
-		return nil, NewAuthServiceError("REQUEST_BUILD_FAILED", "Failed to create request", err)
+		return nil, &AuthServiceError{"REQUEST_BUILD_FAILED", "Failed to create request", err}
 	}
 
 	// Set headers
