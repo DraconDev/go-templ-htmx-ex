574adf546d516f9591c31d8b6a3c254f13405e32
DraconDev <dracsharp@gmail.com>
2025-11-17 22:36:31 +0000

    refactor(server): Update code counter report HTML The HTML file for the code counter report has been updated. This commit includes changes to the styling and structure of the report, as well as updates to the embedded JavaScript for better functionality and theme handling.

---
+++ internal/handlers/auth/user_info.go
@@ -0,0 +1,37 @@
+package auth
+
+import (
+	"net/http"
+
+	"github.com/DraconDev/go-templ-htmx-ex/internal/utils/session"
+	"github.com/DraconDev/go-templ-htmx-ex/templates/layouts"
+)
+
+// GetUserInfo returns current user information for server-side rendering
+// This function is responsible ONLY for extracting user info from session cookies
+func (h *AuthHandler) GetUserInfo(r *http.Request) layouts.UserInfo {
+	// Use session utility to get session cookie
+	sessionID, err := session.GetSessionCookie(r)
+	if err != nil {
+		return layouts.UserInfo{LoggedIn: false}
+	}
+
+	// Get user info from auth microservice
+	userResp, err := h.AuthService.GetUserInfo(sessionID)
+	if err != nil {
+		return layouts.UserInfo{LoggedIn: false}
+	}
+
+	return layouts.UserInfo{
+		LoggedIn: userResp.Success,
+		Name:     userResp.Name,
+		Email:    userResp.Email,
+		Picture:  userResp.Picture,
+	}
+}
+
+// IsUserLoggedIn checks if a user is currently logged in
+// This function is responsible ONLY for checking login status
+func (h *AuthHandler) IsUserLoggedIn(r *http.Request) bool {
+	return session.IsSessionValid(r)
+}
